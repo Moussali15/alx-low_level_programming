c1ea8ede0741844e1dc5ef86be98c128f5914c63
Moussali15 <soukainanajah49@gmail.com>
2023-09-19 06:52:57 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int Digit2, Digit1;
+for (Digit1 = 0; Digit1 <= 99; Digit1++)
+{
+for (Digit2 = 0; Digit2 <= 99; Digit2++)
+{
+if (Digit1 < Digit2 && Digit1 != Digit2)
+{
+putchar((Digit1 / 10) + '0');
+putchar((Digit1 % 10) + '0');
+putchar(' ');
+putchar((Digit2 / 10) + '0');
+putchar((Digit2 % 10) + '0');
+if (Digit1 != 98 || Digit2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
